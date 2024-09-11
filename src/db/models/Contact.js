9ae5249c7787dc40daa931b9fd403b9dd9b4bae9
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-09-11 15:35:49 +0300

    Initialize hw2-mongodb

---
+++ src/db/models/Contact.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+
+const contactSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+
+	},
+	email: {
+		type: String,
+		required: false,
+
+	},
+	isFavourite: {
+		type: Boolean,
+		default: false,
+	},
+	contactType: {
+		type: String,
+		enum: ['work', 'home', 'personal'],
+		required: true,
+		default: 'personal',
+	},
+	createdAt: {
+		type: String,
+		required: true,
+	},
+	updatedAt: {
+		type: String,
+		required: true,
+	}
+
+}, { versionKey: false, timestamps: true });
+
+const ContactModel = model('contact', contactSchema);
+
+export default ContactModel;
